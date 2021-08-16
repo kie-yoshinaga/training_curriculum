5248e7f48c54a6f33d4ab3974cff6628ad0fbe62
kie-yoshinaga <kie.103.eiwa@gmail.com>
2021-08-16 18:59:35 +0900

    曜日反映の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -21,10 +21,7 @@ class CalendarsController < ApplicationController
   def get_week
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
-
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
+#wdays[0]と書くと日曜が反映される
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
@@ -41,16 +38,13 @@ class CalendarsController < ApplicationController
       end
 
 
-      wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
-      if #「wday_numが7以上の場合」という条件式
-        @wday_num = wday_num -7
+      wday_num = Date.today.wday + x# wdayメソッドを用いて取得した数値
+      if wday_num >= 7#「wday_numが7以上の場合」という条件式
+         wday_num = wday_num - 7
       end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: (@wday_num + x)}
+      
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}}
-
     end
 
   end
